91925cd5d8bf02941bce3155da15192874d35902
zhangzhangrp <995405279@qq.com>
2018-03-23 21:15:19 +0800

    3.23

--- js/index.js
+++ js/index.js
@@ -15,5 +15,25 @@ require(["jquery-1.11.2", "carousel"], function($, Carousel){
 $(function(){
     $("#top").on("click",function(){
         $("html").animate({scrollTop:0},500);
-    })
+    });
+
+    var entries = [ 
+        { label: 'HTML', url: 'http://codepen.io/', target: '_top' },
+        { label: 'CSS', url: 'http://threejs.org/', target: '_top' },
+        { label: '数据结构', url: 'http://jscompress.com/', target: '_top' },
+        { label: 'JavaScript', url: 'https://tinypng.com/', target: '_top' },
+        { label: 'Can I Use', url: 'http://caniuse.com/', target: '_top' },
+        { label: 'URL shortener', url: 'https://goo.gl/', target: '_top' },
+        { label: 'Twitter', url: 'https://twitter.com/', target: '_top' },
+        { label: 'Gulp', url: 'http://gulpjs.com/', target: '_top' },
+        { label: 'Browsersync', url: 'https://www.browsersync.io/', target: '_top' },
+        { label: 'GitHub', url: 'https://github.com/', target: '_top' },
+        { label: 'Shadertoy', url: 'https://www.shadertoy.com/', target: '_top' },
+        { label: 'jsPerf', url: 'http://jsperf.com/', target: '_top' },
+        { label: 'Foundation', url: 'http://foundation.zurb.com/', target: '_top' },
+        { label: 'CreateJS', url: 'http://createjs.com/', target: '_top' },
+        { label: 'Velocity.js', url: 'http://julian.com/research/velocity/', target: '_top' },
+        { label: 'jQuery', url: 'https://jquery.com/', target: '_top' },
+    ];  
+    $( '#tag-cloud' ).svg3DTagCloud( {entries: entries} );   
 })
